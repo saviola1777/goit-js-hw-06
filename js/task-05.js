33a9ed7f06cebefa0d8b2bc179325c5a20b50b50
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2022-11-13 16:46:18 +0200

    change-dz6

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ output: document.querySelector('#name-output'),
 }
 refs.inputEl.addEventListener('input', onInputGange);
 function onInputGange(event) {
-  refs.inputEl.value.trim()=== '' ? (refs.output.textContent = "Anonymous") : (refs.output.textContent = event.currentTarget.value)
+  refs.inputEl.value.trim()=== '' ? (refs.output.textContent = "Anonymous") : (refs.output.textContent = event.currentTarget.value.trim())
 }
   
 
--- js/task-08.js
+++ js/task-08.js
@@ -7,8 +7,13 @@ event.preventDefault();
   const { elements: { email, password } } = event.currentTarget;
  if (email.value === '' || password.value === '') {
 return alert('Заповніть всі поля')
- }
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  }  
+  const userData = {
+  email: email.value,
+  password:password.value,
+  }
+  console.log(userData)
+  // console.log(`Login: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
 
